2a6fc768f92ba697b617f96352161586c5cfa2f0
Jarom Santiago <jaromvsantiago@gmail.com>
2022-04-14 00:09:48 -0600

    wheel for orangeSquare not running out of square

--- dj-js-event-listener-practice/index.js
+++ dj-js-event-listener-practice/index.js
@@ -1,4 +1,27 @@
 var djSquare = document.getElementById("square")
 
 djSquare.addEventListener("mouseout", defaultSquare)
+djSquare.addEventListener("mouseover", blueSquare)
+djSquare.addEventListener("mousedown", redSquare)
+djSquare.addEventListener("mouseup", yellowSquare)
+djSquare.addEventListener("dblclick", greenSquare)
+djSquare.addEventListener("wheel", orangeSquare)
 
+function blueSquare (){
+    djSquare.style.backgroundColor = "blue"
+}
+function redSquare (){
+    djSquare.style.backgroundColor = "red"
+}
+function yellowSquare (){
+   djSquare.style.backgroundColor = "yellow"
+}
+function greenSquare (){
+    djSquare.style.backgroundColor = "green"
+}
+function orangeSquare (){
+    djSquare.style.backgroundColor = "orange"
+}
+function defaultSquare (){
+    djSquare.style.backgroundColor = "pink"
+}
