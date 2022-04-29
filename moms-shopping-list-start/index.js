a24378e21c75f3ac64b3da36aa0acee8bae4aea9
Jarom Santiago <jaromvsantiago@gmail.com>
2022-04-29 12:00:46 -0600

    Author name added

--- moms-shopping-list-start/index.js
+++ moms-shopping-list-start/index.js
@@ -7,24 +7,62 @@ item.addEventListener("submit",  (event) => {
     item.title.value = ""
     console.log(addedItem)
 
-    var h1 = document.createElement("li")
-    var editBtn = document.getElementById("editBtn")
-    var exBtn = document.getElementById("exBtn")
-    h1.id = "newItem"
-    h1.textContent = addedItem
-
- 
-    document.getElementsByTagName("ul")[0].append(h1)
-    document.getElementsByTagName("ul")[0].append(editBtn)
-    document.getElementsByTagName("ul")[0].append(exBtn)
-
-
-    exBtn.addEventListener("click", (event) => {
-        exBtn.style.display = "none"
-        h1.style.display = "none"
+    var h1 = createNewItem()
+    var editBtn = createEdit()
+    editBtn.addEventListener("click", event => {
+        var newInput = document.createElement("input")
+        h1.append(newInput)
+        newInput.value = h1.children[0].textContent
+        var submitBtn = document.createElement("button")
+        submitBtn.textContent = "submit"
+        h1.append(submitBtn)
+        console.log(h1.children)
+        submitBtn.addEventListener("click", event =>{
+            h1.children[0].textContent = newInput.value
+            newInput.remove()
+            submitBtn.remove()
+        })
     })
+    var exBtn = document.createElement("button")
+    exBtn.textContent = "X"
+    exBtn.addEventListener("click", event => {
+        var groceryList = document.getElementById("theList")
+        groceryList.removeChild(h1)
+    })
+    h1.id = "newItem"
+    var listSpan = document.createElement("span")
+    listSpan.textContent = addedItem
+    h1.append(listSpan, editBtn, exBtn)
+
+
+    document.getElementById("theList").append(h1)
 
 })
 
+function createNewItem(){
+    var newList = document.createElement("li")
+    return newList
+}
 
+function createEdit(){
+    var editButton = document.createElement("button")
+    editButton.textContent = "edit"
+    return editButton
+}
 
+function deleteButton(){
+    var deleteBtn = document.createElement("button")
+    deleteBtn.textContent = "X"
+    deleteBtn.addEventListener("click", (event) => {
+        var groceryList = document.getElementById("theList")
+        var newGrocery = document.getElementById("newItem")
+        groceryList.removeChild(newGrocery.value)
+    })
+    return deleteBtn
+}
+function removeItem(event){
+    console.log("Item removed")
+    var removeNew = document.getElementsByTagName("ul")[0]
+    removeNew.removeChild(item)
+
+}
