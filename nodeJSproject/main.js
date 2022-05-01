2015862f5c79862cab3fc3a70fb4bf614c7e22a9
Jarom Santiago <jaromvsantiago@gmail.com>
2022-05-01 03:47:40 -0600

    All functions complete

---
+++ nodeJSproject/main.js
@@ -0,0 +1,19 @@
+var readlineSync = require("readline-sync")
+var num1 = readlineSync.question("Please select your first number: ")
+var num2 = readlineSync.question("Please select your second number: ")
+var operation = ["add", "subtract", "multiply", "divide"]
+index = readlineSync.keyInSelect(operation, "Choose an operation")
+
+
+if (operation[index] === "add") {
+    console.log("The result is: ", Number(num1) + Number(num2))
+} 
+if (operation[index] === "subtract"){
+    console.log("The result is: ", Number(num1) - Number(num2))
+}
+if (operation[index] === "divide"){
+    console.log("The result is: ", Number(num1) / Number(num2))
+}
+if (operation[index] === "multiply"){
+    console.log("The result is: ", Number(num1) * Number(num2))
+}
