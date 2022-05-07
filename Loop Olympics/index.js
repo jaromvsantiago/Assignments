f79c3f0b1a7019bd82bd7042cb7e2d829b04c5e3
Jarom Santiago <jaromvsantiago@gmail.com>
2022-05-07 01:31:54 -0600

    Reverse method edited

--- Loop Olympics/index.js
+++ Loop Olympics/index.js
@@ -1,18 +1,21 @@
 //Preliminaries and Bronze #1
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var numbers = []
 var superNumbers = []
 var fruit = ["banana", "orange", "apple", "kiwi"]
 for(var i = 0; i <= 9; i++){
-    superNumbers.push(i);
+    numbers.push(i);
 }
-for(var i = numbers.length - 1; i >= 0; i--){
+console.log(numbers)
+var reverseNumbers = numbers.reverse()
+console.log(reverseNumbers)
+/*for(var i = numbers.length - 1; i >= 0; i--){
     console.log(i)
-}
-numbers.push(superNumbers)
+} Other way to reverse array*/
+superNumbers.push(numbers)
 for(var i = 0; i < fruit.length; i++){
     console.log(fruit[i])
 }
-console.log(superNumbers)
+//console.log(superNumbers)
 
 //Bronze
 var numHundo = []
@@ -24,7 +27,8 @@ for(var i = 0; i <= 100; i++){
 console.log(numHundo)
 var fruitTwo = ["banana", "orange", "apple", "kiwi", "pear", "peach"]
 for(i = 0; i <= fruitTwo.length; i += 2){
-    if(i % 2 === 0){
+    if(fruitTwo.length % 2 === 0){
+        //fruitTwo.push(i)
         console.log(fruitTwo[i])
     }
 }
